d265ddda4eeeb44fefc02aa4cb5431e0ecb1d0e2
seasons986 <annalin12345@gmail.com>
2024-02-03 16:00:36 -0500

    updated

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   const container = document.getElementById("container");
+  // <input type="range" id="volume" name="volume" min="0" max="11" />
+  // <label for="volume">Volume</label>
+
+  const input = document.createElement("input");
+  input.type = "range";
+  input.min = "0";
+  input.max = "1000";
+
+  window.onscroll = () => input.value++;
 });
